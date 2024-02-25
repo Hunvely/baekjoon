fe89b070d17be579148b8b26a61c8f27ad520cdd
Hunvely <jihoon9611@gmail.com>
2024-02-25 19:30:39 +0900

    3052 Remainder code

---
+++ Remainder.java
@@ -0,0 +1,33 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Remainder {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        
+        int[] remainArr = new int[10];
+        boolean bl;
+        int count = 0;
+
+        for (int i = 0; i < remainArr.length; i++) {
+            remainArr[i] = input.nextInt() % 42;
+        }
+
+        for (int i = 0; i < remainArr.length; i++) {
+            bl = false;
+            for (int j = i + 1; j < remainArr.length; j++) {
+                if (remainArr[i] == remainArr[j]) {
+                    bl = true;
+                    break;
+                }
+            }
+            if (bl == false) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
